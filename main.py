fff3d094ce9dd2bcf02142d1893dde419e4cb4c3
Evgn22 <rezonans2352@mail.ru>
2023-03-30 19:49:32 +0900

    начал проработку функции изменения спец символа

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import discord
 import logging
+import os
+import asyncio
+import datetime
+from discord.ext import commands
+from discord.ext import commands, tasks
+from discord.utils import get
+from discord.ext.commands import Bot
+import sys
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -7,7 +15,7 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-TOKEN = "MTA4ODA0Nzc1NjQ4MTkyNTE1MQ.GiT04Q.c3gspoc01AnmZvpYLo-Sf7Op2n0maFFtF5ossI"
+TOKEN = "MTA4ODA0Nzc1NjQ4MTkyNTE1MQ.GH5v9B.5iP48faD3VKirPpg4WkS4y277K2bwoiGook0FY"
 
 
 class YLBotClient(discord.Client):
@@ -27,10 +35,14 @@ class YLBotClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if "привет" in message.content.lower():
-            await message.channel.send("И тебе привет")
-        else:
-            await message.channel.send("Спасибо за сообщение")
+        symbol = open('prefix.txt', 'r').readline(1)
+        if symbol == message.content.split()[0][0]:
+            if f'{symbol}сменить префикс на' == ' '.join(message.content.split()[:-1]):
+                with open('prefix.txt', 'w') as f:
+                    f.write(message.content.split()[-1])
+                await message.channel.send(f'префикс сменён на {message.content.split()[-1]}')
+            else:
+                await message.channel.send("Спасибо за сообщение")
 
 
 intents = discord.Intents.default()
