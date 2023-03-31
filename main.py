7fd3473f21816e06fc1f4e7f97b3b53094410ed6
Evgn22 <rezonans2352@mail.ru>
2023-03-31 14:28:35 +0900

    продолжается работа над префиксом

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import logging
+import sqlite3
 import os
 import asyncio
 import datetime
@@ -25,6 +26,14 @@ class YLBotClient(discord.Client):
             logger.info(
                 f'{self.user} подключились к чату:\n'
                 f'{guild.name}(id: {guild.id})')
+            # con = sqlite3.connect('Servers_data.db')
+            # cur = con.cursor()
+            # ids = cur.execute("""SELECT server_id FROM servers_prefix""").fetchall()
+            # if guild.id not in ids:
+            #     cur.execute(f"""INSERT INTO servers_prefix(server_id, prefix)
+            #                 VALUES({guild.id}, {'!'})""").fetchall()
+            #     con.commit()
+            #     con.close()
 
     async def on_member_join(self, member):
         await member.create_dm()
@@ -35,11 +44,19 @@ class YLBotClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        symbol = open('prefix.txt', 'r').readline(1)
+        con = sqlite3.connect('Servers_data.db')
+        cur = con.cursor()
+        symbol = cur.execute(f"""SELECT prefix FROM servers_prefix
+                             WHERE server_id LIKE {message.guild.id}""").fetchall()
+        symbol = symbol[0][0]
         if symbol == message.content.split()[0][0]:
             if f'{symbol}сменить префикс на' == ' '.join(message.content.split()[:-1]):
-                with open('prefix.txt', 'w') as f:
-                    f.write(message.content.split()[-1])
+                print(message.guild.id)
+                cur.execute(f"""UPDATE servers_prefix
+                            SET prefix = {message.content.split()[-1]}
+                            WHERE server_id LIKE {message.guild.id}""").fetchall()
+                con.commit()
+                con.close()
                 await message.channel.send(f'префикс сменён на {message.content.split()[-1]}')
             else:
                 await message.channel.send("Спасибо за сообщение")
